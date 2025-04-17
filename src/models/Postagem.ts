72e9d16616cb44a7b671bcb5476ad977109b4c14
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-04-16 22:33:47 -0300

    Implementação do modelo de postagem

---
+++ src/models/Postagem.ts
@@ -0,0 +1,28 @@
+import { randomUUID } from "node:crypto";
+import Autor from "./Autor";
+
+type Tpost = {
+  titulo: string;
+  descricao: string;
+  autor: Autor;
+};
+
+export default class Post {
+  readonly id: string;
+  titulo: string;
+  descricao: string;
+  autor: Autor;
+  data_criacao: Date;
+
+  constructor(post: Tpost) {
+    this.id = this.gerarId();
+    this.titulo = post.titulo;
+    this.descricao = post.descricao;
+    this.autor = post.autor;
+    this.data_criacao = new Date();
+  }
+
+  private gerarId(): string {
+    return randomUUID();
+  }
+}
